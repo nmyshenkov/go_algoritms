dfb2584a1bf71ab2130c1123d7cd7ba32803b97f
nmyshenkov <nikita11e@gmail.com>
2019-09-30 20:20:40 +0300

    added binary search in sorted array

--- search/search.go
+++ search/search.go
@@ -1,6 +1,6 @@
 package search
 
-// linear - linear search on int array
+// linear - linear search in int array
 func linear(el int, arr []int) bool {
 
 	for _, val := range arr {
@@ -8,5 +8,46 @@ func linear(el int, arr []int) bool {
 			return true
 		}
 	}
+	return false
+}
+
+// binary - binary search in sorted int array
+func binary(el int, arr []int) bool {
+	// check if arr len eq 1
+	if len(arr) == 1 {
+		if arr[0] == el {
+			return true
+		}
+		return false
+	}
+
+	var (
+		left, mid, right int
+	)
+
+	right = len(arr)
+
+	// check sort type and flip if it need
+	if arr[0] > arr[len(arr)-1] {
+		for i := len(arr)/2 - 1; i >= 0; i-- {
+			opp := len(arr) - 1 - i
+			arr[i], arr[opp] = arr[opp], arr[i]
+		}
+	}
+
+	for !(left >= right) {
+		mid = left + (right-left)/2
+
+		if arr[mid] == el {
+			return true
+		}
+
+		if arr[mid] > el {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
 	return false
 }
--- search/search_test.go
+++ search/search_test.go
@@ -2,7 +2,10 @@ package search
 
 import "testing"
 
-var arr = []int{3, 15, 23, 51, 20, 10, 11, 65, 5, 8, 0, 22}
+var (
+	arr       = []int{3, 15, 23, 51, 20, 10, 11, 65, 5, 8, 0, 22}
+	sortedArr = []int{0, 3, 5, 8, 10, 11, 15, 20, 22, 23, 51, 65}
+)
 
 func Test_linear(t *testing.T) {
 	type args struct {
@@ -16,6 +19,8 @@ func Test_linear(t *testing.T) {
 	}{
 		{name: "Test 1", args: args{el: 8, arr: arr}, want: true},
 		{name: "Test 2", args: args{el: 7, arr: arr}, want: false},
+		{name: "Test 4", args: args{el: 8, arr: sortedArr}, want: true},
+		{name: "Test 5", args: args{el: 7, arr: sortedArr}, want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -23,5 +28,39 @@ func Test_linear(t *testing.T) {
 				t.Errorf("linear() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func Test_binary(t *testing.T) {
+	type args struct {
+		el  int
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "Test 4", args: args{el: 8, arr: sortedArr}, want: true},
+		{name: "Test 5", args: args{el: 7, arr: sortedArr}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := binary(tt.args.el, tt.args.arr); got != tt.want {
+				t.Errorf("binary() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Benchmark_linear(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		linear(23, sortedArr)
+	}
+}
+
+func Benchmark_binary(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		binary(23, sortedArr)
 	}
 }
